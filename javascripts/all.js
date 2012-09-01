bfc69435685f40690725d42818a69a80c6d6856b
Sander van de Graaf <mail@svdgraaf.nl>
2012-09-01 21:27:21 +0200

    added hash breaker for setting proxy

--- javascripts/all.js
+++ javascripts/all.js
@@ -3235,6 +3235,27 @@ SmoothieChart.prototype.render = function(canvas, time) {
     _init();
 
 })();
+$(function() {
+	// check for hashbang
+	if(window.location.hash) {
+		// break hash
+		hash = window.location.hash;
+		hash = hash.replace('#','');
+		hash = hash.split('&');
+		for(rule in hash){
+			rule = hash[rule];
+			rule = rule.split('=');
+			switch(rule[0]){
+				case 'set-proxy':
+					$.jStorage.set('proxy-location', rule[1]);
+					console.log(rule[1]);
+			}
+		}
+
+		
+	}
+});
+
 
 
 
